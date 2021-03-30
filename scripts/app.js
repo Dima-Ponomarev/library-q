a5ee09329d6faca625198e468de367c73fe11d50
Dima Ponomarev <kikkyow18@gmail.com>
2021-03-30 17:48:11 +0400

    feat: add book status toggle

--- scripts/app.js
+++ scripts/app.js
@@ -8,8 +8,8 @@ const writeBtn = document.querySelector('.side-controls__add-written-book')
 const mainList = document.querySelector('.main-list')
 let books = [];
 
-class Book{
-    constructor(title, text, statusRead, mainRoot, number){
+class Book {
+    constructor(title, text, statusRead, mainRoot, number) {
         this.title = title;
         this.text = text;
         this.statusRead = statusRead;
@@ -18,54 +18,84 @@ class Book{
 
     }
 
-    toJSON(){
-        return{
+    toJSON() {
+        return {
             title: this.title,
             text: this.text,
             statusRead: this.statusRead,
         }
     }
 
-    setNewTitle(newTitle){
+    setNewTitle(newTitle) {
         const renderedElementTitle = document.querySelector(`[data-number='${this.number}']`).childNodes[0].childNodes[0];
         renderedElementTitle.innerText = newTitle;
         this.title = newTitle;
     }
 
-    setNewNumber(num){
+    setNewNumber(num) {
         const renderedElement = document.querySelector(`[data-number='${this.number}']`);
         renderedElement.dataset.number = num;
         this.number = num;
     }
 
-    setStatusRead(bool){
-        this.statusRead = bool;
+    toggleStatus() {
+        const renderedElementStatus = document.querySelector(`[data-number='${this.number}']`).childNodes[0].childNodes[1];
+        const renderedElementStatusBtn = document.querySelector(`[data-number='${this.number}']`).childNodes[1].childNodes[0];
+        if (this.statusRead) {
+            renderedElementStatus.classList.remove('book__status--read');
+            renderedElementStatus.innerText = 'Book is not read';
+            renderedElementStatusBtn.classList.remove('book__status-btn--read');
+            this.statusRead = false;
+        } else {
+            renderedElementStatus.classList.add('book__status--read');
+            renderedElementStatus.innerText = 'Book is read';
+            renderedElementStatusBtn.classList.add('book__status-btn--read');
+            this.statusRead = true;
+        }
     }
 
-    renderToList(root){
+    renderToList(root) {
         const listItem = document.createElement('li');
         listItem.className = `${root.className}__item book`;
         listItem.dataset.number = this.number
-        
+
         //title wrap
         const titleWrap = document.createElement('div');
         titleWrap.className = 'book__title-wrap'
-        
+
         //book title element
         const bookTitle = document.createElement('p');
         bookTitle.className = 'book__title'
-        if (this.title.length >= 25){
+        if (this.title.length >= 25) {
             bookTitle.innerText = this.title.substring(0, 23) + '...';
         } else {
             bookTitle.innerText = this.title;
         }
 
+        const bookStatus = document.createElement('p');
+        bookStatus.className = 'book__status'
+        if (this.statusRead) {
+            bookStatus.classList.add('book__status--read');
+            bookStatus.innerText = 'Book is read';
+        } else {
+            bookStatus.innerText = 'Book is not read';
+        }
+
         //append title elements to title wrap
         titleWrap.appendChild(bookTitle);
+        titleWrap.appendChild(bookStatus);
 
         //controls wrap
         const controlBtns = document.createElement('div');
         controlBtns.className = 'book__btn-wrap';
+
+        //toggle book status btn
+        const statusBtn = document.createElement('button');
+        statusBtn.className = 'book__status-btn';
+        statusBtn.addEventListener('click', e => {
+            e.stopPropagation();
+            toggleStatusHandler(this);
+        })
 
         //edit btn
         const editBtn = document.createElement('button');
@@ -79,12 +109,13 @@ class Book{
         //delete btn
         const deleteBtn = document.createElement('button');
         deleteBtn.className = 'book__delete-btn';
-        deleteBtn.addEventListener('click', e =>{
+        deleteBtn.addEventListener('click', e => {
             e.stopPropagation();
             deleteBookHandler(this);
         })
-        
+
         //append cntrol element to control panel
+        controlBtns.appendChild(statusBtn);
         controlBtns.appendChild(editBtn);
         controlBtns.appendChild(deleteBtn);
 
@@ -107,17 +138,17 @@ const renderEditPopup = (book) => {
     const popupElement = document.querySelector('.popup');
     const popupContainer = document.createElement('div');
     popupContainer.className = 'popup__container';
-    popupContainer.innerHTML = 
-    `
+    popupContainer.innerHTML =
+        `
     <label class="popup__label" for="p-title">Book title:</label>
     <input type="text" name="title" id="p-title" class="popup__title-input" value="${book.title}">
     <textarea name="book-text" cols="50" rows="10" id="p-text" class="popup__book-text">${book.text}</textarea>
     `
     const saveBtn = document.createElement('button');
-    saveBtn.className =  'popup__save-btn btn';
+    saveBtn.className = 'popup__save-btn btn';
     saveBtn.innerText = 'Save';
     saveBtn.addEventListener('click', e => {
-        saveBtnHandler(book)       
+        saveBtnHandler(book)
     })
 
     popupContainer.appendChild(saveBtn);
@@ -131,7 +162,7 @@ const saveBtnHandler = (book) => {
     const text = document.getElementById('p-text');
     //find and update book values
     books.forEach(item => {
-        if (book === item){
+        if (book === item) {
             item.setNewTitle(title.value);
             item.text = text.value
         }
@@ -148,6 +179,19 @@ const deleteBookHandler = (book) => {
     const deleteBookDOM = document.querySelector(`[data-number = "${book.number}"]`);
     deleteBookDOM.remove();
     books.splice(book.number, 1);
+    books.forEach((item, index) => {
+        item.setNewNumber(index)
+    })
+    updateLocal(books);
+}
+
+//-----------------TOGGLE STATUS--------------------
+const toggleStatusHandler = (book) => {
+    books.forEach(item => {
+        if (item === book) {
+            item.toggleStatus();
+        }
+    });
     updateLocal(books);
 }
 
@@ -172,13 +216,13 @@ const openNewBookHandler = (book) => {
 
 //---------------LOCAL STORAGE HANDLING-----------------
 
-const updateLocal = (books) =>{
+const updateLocal = (books) => {
     localStorage.setItem('books', JSON.stringify(books));
 }
 
-const saveToLocal = (book) =>{
+const saveToLocal = (book) => {
     let localBooks;
-    if (localStorage.getItem('books') === null){
+    if (localStorage.getItem('books') === null) {
         localBooks = [];
     } else {
         localBooks = JSON.parse(localStorage.getItem('books'))
@@ -189,9 +233,9 @@ const saveToLocal = (book) =>{
 
 const getLocal = () => {
     const loadedBooks = [];
-    if (localStorage.getItem('books') !== null){
+    if (localStorage.getItem('books') !== null) {
         const bookObjects = JSON.parse(localStorage.getItem('books'));
-        bookObjects.forEach((obj, index) =>{
+        bookObjects.forEach((obj, index) => {
             loadedBooks.push(new Book(obj.title, obj.text, obj.statusRead, mainList, index))
         })
     }
@@ -202,8 +246,8 @@ const getLocal = () => {
 
 //---------------------SWITCH BETWEEN 2 FORMS--------------------------
 
-const uploadSwitchHandler = (e) =>{
-    if (!e.target.classList.contains('btn--active')){
+const uploadSwitchHandler = (e) => {
+    if (!e.target.classList.contains('btn--active')) {
         e.target.classList.add('btn--active');
         writeSwitchBtn.classList.remove('btn--active');
         formWrap.forEach(form => form.classList.toggle('side-controls__form-wrap--active'));
@@ -211,8 +255,8 @@ const uploadSwitchHandler = (e) =>{
 }
 
 
-const writeSwitchHandler = (e) =>{
-    if (!e.target.classList.contains('btn--active')){
+const writeSwitchHandler = (e) => {
+    if (!e.target.classList.contains('btn--active')) {
         e.target.classList.add('btn--active');
         uploadSwitchBtn.classList.remove('btn--active');
         formWrap.forEach(form => form.classList.toggle('side-controls__form-wrap--active'));
@@ -233,12 +277,11 @@ writeBtn.addEventListener('click', (e) => {
     const newBook = new Book(title.value, text.value, false, mainList, books.length);
     books.push(newBook);
     saveToLocal(newBook);
-    console.log(books);
     title.value = '';
     text.value = '';
 })
 
-uploadForm.addEventListener('submit', function (e){
+uploadForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const file = document.getElementById('file').files[0];
     const title = document.getElementById('up-title');
@@ -247,7 +290,7 @@ uploadForm.addEventListener('submit', function (e){
     fetch('https://apiinterns.osora.ru/', {
         method: 'post',
         body: formData
-    }).then(function(response) {
+    }).then(function (response) {
         return response.json();
     }).then(function (json) {
         const newBook = new Book(title.value, json.text, false, mainList, books.length);
@@ -255,7 +298,7 @@ uploadForm.addEventListener('submit', function (e){
         saveToLocal(newBook);
         console.log(books);
         title.value = '';
-    }).catch(function (error){
+    }).catch(function (error) {
         console.error(error)
     })
 
